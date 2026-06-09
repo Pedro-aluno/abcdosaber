e9d45569e31ad22f49598b7928fdad521d44bd96
Pedro-Aluno <pedrohpires2023@gmail.com>
2026-06-08 21:52:48 -0300

    Aula 08/06 admin (titulo,aluno) models(titulo,aluno)

--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    matricula = models.AutoField(
+        primary_key=True,
+        help_text="Matrícula do aluno",
+        
+    )
+
+    nome = models.CharField(
+        max_length=70,
+        null = False,
+        help_text="Informe o nome do aluno",
+    )
+    
+    data_inicial = models.DateField(
+        null=False,
+        help_text="Informe que horário o aluno começou a estudar",
+    )
+
+    data_final = models.DateField(
+        help_text="Informe que horário o aluno terminou de estudar",
+        null=True,
+    )
+    def __str__(self):
+        return f"{self.matricula} {self.nome} {self.data_inicial} {self.data_final}"
